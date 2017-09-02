6a8c1004ac6e74195e5380eb7755c64034df72a5
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-09-01 22:23:48 -0300

    add checker to check when element is visible / hidden

--- is.js
+++ is.js
@@ -8,11 +8,21 @@
     return this.parentNode.lastChild === this;
   };
 
+  var isVisible = function() {
+    return !!( this.offsetWith || this.offsetHeight || this.getClientRects().length );
+  };
+
+  isHidden = function() {
+    return !isVisible.call(this);
+  };
+
   N.is.prototype = function(value) {
     if (value.indexOf(':') === 0) {
       var options = {
         'first-child': isFirstChild,
-        'last-child': isLastChild
+        'last-child': isLastChild,
+        'visible': isVisible,
+        'hidden': isHidden
       };
 
       return options[value.split(':')[1]].call(this);
